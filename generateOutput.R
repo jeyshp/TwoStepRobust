226bf41788a8b26d44962f243b817057c2e8a134
jeyshp <jeyshp@gmail.com>
2023-06-20 13:25:16 -0700

    Update: generateOutput.R
    Comments added

--- generateOutput.R
+++ generateOutput.R
@@ -1,26 +1,40 @@
+#' @description generateOutput() calls simfunc() in parallel for varying values of p.active and contamination prop
+#' 
+#' @param N Number of training sets 
+#' @param n Sample size for training set
+#' @param m Sample size for test set
+#' @param p Total number of parameters
+#' @param rho Correlation within a block of active parameters
+#' @param rho.inactive Correlation between blocks of active parameters
+#' @param p.active Number of active parameters
+#' @param group.size Size of one block of active parameters
+#' @param snr Signal to noise ratio
+#' @param contamination.prop Contamination proportion
+#' @param n_models Number of models for ensemble 
 
+#Required libraries
 library("parallel")
-install.packages("doSNOW")
 library("doSNOW")
-
-#generateOutput() calls simfunc in parallel for varying values of p.active, snr, contamination prop and rho
 
 source("simfunc.R")
 
-generateOutput <- function (N, #number of training sets
-                            n , #sample size for training set
-                            m, #sample size for test set
-                            p, #number of parameters
-                            rho, #correlation
+generateOutput <- function (N, 
+                            n , 
+                            m, 
+                            p, 
+                            rho, 
                             rho.inactive = 0.2,
-                            p.active, #number of active parameters
+                            p.active, 
                             group.size,
                             snr, 
-                            contamination.prop, #contamination proportion
+                            contamination.prop, 
                             seed = 0,
                             n_models,
                             ...){
+  #Setting seed
   set.seed(seed)
+  
+  #Initializing number of cores used 
   if (length(p.active)*length(contamination.prop) != 1) {
     n_clusters =  min(length(p.active)*length(contamination.prop),detectCores()-1)
   } else
@@ -28,8 +42,7 @@ generateOutput <- function (N, #number of training sets
   
   mycluster = makeCluster(n_clusters)
   
-  #present results as table (fixed(n.p), fixed(rho,snr)) - varying prop of active p and cont.prop
-  #saving files 
+  #Parallel computation of simfunc() for different values of p.active and contamination proportion 
   results = foreach(mycontprop = contamination.prop, .packages = c("pense","robustHD","mvnfast","robStepSplitReg","hqreg","glmnet")) %:%
     foreach(mypactive = p.active, .packages = c("pense","robustHD","mvnfast","robStepSplitReg","hqreg","glmnet")) %dopar% {
       output = simfunc(N = N, n=n, m=m, p=p, rho = rho, rho.inactive = rho.inactive, p.active = mypactive, 
