6386c8da6b6947c9d05b586f39134d7b367d111f
jeyshp <jeyshp@gmail.com>
2023-06-20 13:26:05 -0700

    Update to simfunc.R
    Comments added

--- simfunc.R
+++ simfunc.R
@@ -1,21 +1,33 @@
-#simfunc() calls generateData() and generatePred() and produces a matrix containing 
+#' @description simfunc() calls generateData() and generatePred() 
+#' 
+#' @param N Number of training sets 
+#' @param n Sample size for training set
+#' @param m Sample size for test set
+#' @param p Total number of parameters
+#' @param rho Correlation within a block of active parameters
+#' @param rho.inactive Correlation between blocks of active parameters
+#' @param p.active Number of active parameters
+#' @param group.size Size of one block of active parameters
+#' @param snr Signal to noise ratio
+#' @param contamination.prop Contamination proportion
+#' @param n_models Number of models for ensemble 
 
 source("generateData.R")
 source("generatePred.R")
 
 
-simfunc <- function(N, #number of training sets
-                    n, #sample size for training set
-                    m, #sample size for test set
-                    parameters, #number of parameters
-                    rho, #correlation
+simfunc <- function(N, 
+                    n, 
+                    m, 
+                    parameters, 
+                    rho, 
                     rho.inactive,
-                    p.active, #number of active parameters
+                    p.active, 
                     group.size,
                     snr, 
                     contamination.prop,
                     n_models,
-                    ...){ #contamination proportion
+                    ...){ 
   
   sim_Data <- generateData(N, n, m, parameters, rho, rho.inactive, p.active, group.size, snr, contamination.prop)
   output <- generatePred(sim_Data, n_models, ...)
